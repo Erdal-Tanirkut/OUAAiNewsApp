ba5bac6cc484acdd91419f10f4874fe050c78de9
Erdal-Tanirkut <159000693+Erdal-Tanirkut@users.noreply.github.com>
2024-05-20 01:27:03 +0300

    project initialized

---
+++ android/app/src/main/kotlin/com/example/newsclass_firebasetrial/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.newsclass_firebasetrial
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/view/askaipage.dart
@@ -0,0 +1,1 @@
+/Users/erdaltanirkut/Users/erdaltanirkut/flutter_projects/newsclass_firebasetrial
